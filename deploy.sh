beef1103973781fed740d7c02d935ee477beb927
Tom Oberhauser <tom@devfoo.de>
2019-01-04 15:21:09 +0100

    Added run and deploy script and k8s stub

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 ./build.sh
+docker build -t registry.beuth-hochschule.de/toberhauser/senteval-k8s .
+docker push registry.beuth-hochschule.de/toberhauser/senteval-k8s
--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-docker run -t --runtime=nvidia \
--e ENCODERURL='http://141.64.184.124:5000/embed/sentences' \    # SET ENCODER URL
--e ENCODERTYPE='TOKEN' \                                        # SET ENCODER MODE (TOKEN, SENTENCE)
--v $(pwd)/results:/root/results \                               # MOUNT RESULTS PATH
-senteval-gpu
+export NV_GPU='0'
+#export NV_GPU='6,7'
+nvidia-docker run \
+-e ENCODERURL='http://datexis-worker15:31022/v2/embed/sentences/top' \
+-e ENCODERTYPE='TOKEN' \
+-v $(pwd)/results:/root/results senteval-gpu
