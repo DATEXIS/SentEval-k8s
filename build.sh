8f18a4905ea3de24976b580fbf0943ebfaf4f358
Tom Oberhauser <tom@devfoo.de>
2019-02-04 16:03:28 +0100

    Added evaluation parameters to result, closes #10.

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 # remove SentEval data if already existing (otherwise download script fails)
 find SentEval/data/downstream/ -mindepth 1 -maxdepth 1 -type d -exec rm -rf {} \;
 
+# Store current git hash to identify build
+git rev-parse HEAD > commit-hash.txt
+
 # Download SentEval downstream task data
 cd SentEval/data/downstream
 bash get_transfer_data.bash
