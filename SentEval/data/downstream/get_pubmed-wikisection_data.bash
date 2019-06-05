b40752ef036926c70dff2731f12784065682eff2
Tom Oberhauser <tom@devfoo.de>
2019-06-05 18:13:15 +0200

    Fix: Wrong download URL for WikiSection and PubMedSection, fixes #37

--- SentEval/data/downstream/get_pubmed-wikisection_data.bash
+++ SentEval/data/downstream/get_pubmed-wikisection_data.bash
@@ -4,12 +4,12 @@ data_path=.
 
 ### PubMedSection
 mkdir $data_path/PubMedSection
-curl -Lo $data_path/PubMedSection/data.tar.xz https://cloud.beuth-hochschule.de/index.php/s/YGWojWKBKg66fnZ
+curl -Lo $data_path/PubMedSection/data.tar.xz https://cloud.beuth-hochschule.de/index.php/s/46ejzpyRi2J9Ao8/download
 tar -xf $data_path/PubMedSection/data.tar.xz -C $data_path/PubMedSection
 rm $data_path/PubMedSection/data.tar.xz
 
 ### WikiSection
 mkdir $data_path/WikiSection
-curl -Lo $data_path/WikiSection/data.tar.xz https://cloud.beuth-hochschule.de/index.php/s/cQJgjcnx4KefXyp
+curl -Lo $data_path/WikiSection/data.tar.xz https://cloud.beuth-hochschule.de/index.php/s/cj6xegeADpsmaTR/download
 tar -xf $data_path/WikiSection/data.tar.xz -C $data_path/WikiSection
 rm $data_path/WikiSection/data.tar.xz
